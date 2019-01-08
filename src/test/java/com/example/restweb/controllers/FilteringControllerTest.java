7b6b110bd3df794399c9a7d6b80f0da2fc78d152
taelee <tae.lee@outlook.com>
2019-01-08 10:10:29 +0100

    Test class UserController added and unused imports removed FilteringsControllerTest

--- src/test/java/com/example/restweb/controllers/FilteringControllerTest.java
+++ src/test/java/com/example/restweb/controllers/FilteringControllerTest.java
@@ -2,9 +2,6 @@ package com.example.restweb.controllers;
 
 import com.example.restweb.model.SomeBean;
 import com.example.restweb.model.SomeBeanStaticFiltering;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,20 +11,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNull;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -49,8 +39,8 @@ class FilteringControllerTest {
     void retrieveSomeBean() throws Exception {
         SomeBeanStaticFiltering someBeanStaticFiltering = restTemplate
                 .getForObject("/filtering-static", SomeBeanStaticFiltering.class);
-        assertEquals(null, someBeanStaticFiltering.getValue1());
-        assertEquals(null, someBeanStaticFiltering.getValue2());
+        assertNull(someBeanStaticFiltering.getValue1());
+        assertNull(someBeanStaticFiltering.getValue2());
         assertEquals("value3", someBeanStaticFiltering.getValue3());
     }
 
@@ -61,11 +51,11 @@ class FilteringControllerTest {
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<SomeBean>>() {});
         List<SomeBean> someBeans = rateResponse.getBody();
 
-        assertEquals(someBeans.get(0).getValue1(), null);
+        assertNull(someBeans.get(0).getValue1());
         assertEquals(someBeans.get(0).getValue2(), "value2");
         assertEquals(someBeans.get(0).getValue3(), "value3");
 
-        assertEquals(someBeans.get(1).getValue1(), null);
+        assertNull(someBeans.get(1).getValue1());
         assertEquals(someBeans.get(1).getValue2(), "value22");
         assertEquals(someBeans.get(1).getValue3(), "value33");
 
@@ -81,6 +71,6 @@ class FilteringControllerTest {
         SomeBean someBean = rateResponse.getBody();
         assertEquals(someBean.getValue1(), "value1");
         assertEquals(someBean.getValue2(), "value2");
-        assertEquals(someBean.getValue3(), null);
+        assertNull(someBean.getValue3());
     }
 }
